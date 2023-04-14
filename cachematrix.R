650546de1b2ca16cf1b3fd733d93d8ff58e6563b
Zhang Miao (IFAG PSS RFS D2 SW ACE) <Miao.Zhang@infineon.com>
2023-04-14 16:40:12 +0200

    first submit

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  res<-NULL
+  set<-function(y){
+    x<<-y
+    res<<-NULL
+  }
+  get<-function() x
+  setinv<-function(inv) res<<-inv
+  getinv<-function() res
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
 
 }
 
@@ -12,4 +21,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv<-x$getinv()
+  if (!is.null(inv)){
+    return(inv)
+  }
+  data<-x$get()
+  inv<-solve(data)
+  x$setinv(inv)
+  
+  inv
+  
 }
